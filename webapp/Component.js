d0fe2f43edd86cbf81a307be446500cddedd47d6
mmarkvillanueva <mmarkvillanueva@gmail.com>
2019-02-17 19:59:46 +0800

    Initial Commit and push

---
+++ webapp/Component.js
@@ -0,0 +1,9 @@
+jQuery.sap.declare("com.dxc.testztest_annotation.Component");
+sap.ui.getCore().loadLibrary("sap.ui.generic.app");
+jQuery.sap.require("sap.ui.generic.app.AppComponent");
+
+sap.ui.generic.app.AppComponent.extend("com.dxc.testztest_annotation.Component", {
+	metadata: {
+		"manifest": "json"
+	}
+});
